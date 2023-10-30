ad2621ea13de2835323f75a8d8a8169033412348
Bitea F.N. Florian-Andrei <cti22a107@cst.uoradea.local>
2023-10-30 19:04:17 +0200

    Lab 3

---
+++ src/ro/uoradea/Dog.java
@@ -0,0 +1,9 @@
+package ro.uoradea;
+
+public class Dog {
+    public Dog(String n, String b) {
+        name = n;
+        breed = b;
+        Dog myObj = new Dog(String n, String b);
+    }
+}
---
+++ src/ro/uoradea/Lab03.java
@@ -0,0 +1,9 @@
+package ro.uoradea;
+
+public class Lab03 {
+    public static void main(String[] args) {
+        new Dog("Fluffy", "Doggy");
+        System.out.println(myObj.name);
+        System.out.println(myObj.breed);
+    }
+}
